8c920701b2acf4e0381f6ff8af2c15dd1fe0cbdf
hen samii <henriquesamii@hotmail.com>
2021-01-09 14:46:57 -0300

    rocket goes up

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // Can thrust while rotating
+        {
+            //print("Space pressed, thrusting");
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("A pressed, going left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("D pressed, going right");
+        }
+    }
+}
