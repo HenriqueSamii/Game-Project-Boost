e50c0ef1d7e56b31fb14f0d80dc1385101a55706
hen samii <henriquesamii@hotmail.com>
2021-01-10 11:28:16 -0300

    rocket with sound fx

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,14 +27,24 @@ public class Rocket : MonoBehaviour
         {
             //print("Space pressed, thrusting");
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
-            print("A pressed, going left");
+            //print("A pressed, going left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("D pressed, going right");
+            //print("D pressed, going right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
